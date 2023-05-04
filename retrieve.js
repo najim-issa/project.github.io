488a9d3a105c3768953906bb6329665412da4e38
najim-issa <najimissaya21@gmail.com>
2023-05-04 18:01:11 +0300

    first commit

---
+++ retrieve.js
@@ -0,0 +1,7 @@
+console.log("Hellow Mella");
+
+
+let name = document.querySelector("#name");
+let address = document.querySelector("#address");
+let email = document.querySelector("#email");
+let comments = document.querySelector("#comments");
--- script.js
+++ script.js
@@ -1,32 +1,53 @@
-function test() {
-    //Retrieving Data
-    var usid = document.getElementById("username").value;
-    var em = document.getElementById("email").value;
-    var pw = document.getElementById("password").value;
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
-    alert(usid + em + pw);
-
-
-    //Storing data in localStorage here 
-
-    var user = localStorage.setItem('usid', usid);
-    var email = localStorage.setItem('em', em);
-    var password = localStorage.setItem('pw', pw);
-
-    /*Retrieving stored data and using it for falculation
-
-    var user = localStorage.getItem('usid', usid);
-    var email = localStorage.getItem('em', em);
-    var password = localStorage.getItem('pw', pw);
-
-    var a, b, c;
-    a = "admin";
-    b = "najimissaya21@gmail.com";
-    c = 12345678;
-
-    if (a == user && b == email && c == password) {
-        alert("login successfull !");
+    if (username.trim() == "" || password.trim() == "") {
+        alert("Please enter your username and password.");
     } else {
-        alert("Invalid details !");
-    }*/
+        localStorage.setItem("username", username);
+        localStorage.setItem("password", password);
+        alert("Login successful.");
+    }
 }
+
+function clearForm() {
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+}
+
+
+/*function test() {
+//Retrieving Data
+var nme = document.getElementById("name").value;
+var em = document.getElementById("address").value;
+var pw = document.getElementById("email").value;
+var cmt = document.getElementById("comments").value;
+
+
+alert(usid + em + pw + cmt);
+
+
+//Storing data in localStorage here 
+
+var name = localStorage.setItem('name', name);
+var email = localStorage.setItem('address', em);
+var password = localStorage.setItem('email', pw);
+var cmte = localStorage.setItem('comments', pw);
+
+/*Retrieving stored data and using it for falculation
+
+var user = localStorage.getItem('usid', usid);
+var email = localStorage.getItem('em', em);
+var password = localStorage.getItem('pw', pw);
+
+var a, b, c;
+a = "admin";
+b = "najimissaya21@gmail.com";
+c = 12345678;
+
+if (a == user && b == email && c == password) {
+    alert("login successfull !");
+} else {
+    alert("Invalid details !");
+}*/
